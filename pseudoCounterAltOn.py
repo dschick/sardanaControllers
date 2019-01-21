14d67f74a41af1a3f60dca767c7935cd49e75795
Daniel Schick <schick.daniel@gmail.com>
2019-01-21 23:08:39 +0100

    change 1:1

--- pseudoCounterAltOn.py
+++ pseudoCounterAltOn.py
@@ -1,26 +1,26 @@
 from sardana.pool.controller import PseudoCounterController
-import PyTango
+from sardana.taurus.core.tango.sardana.pool import registerExtensions
+registerExtensions()
+import taurus
 
 class pseudoCounterAltOn(PseudoCounterController):
-    """ A simple pseudo counter which receives two counter values (I and I0)
-        and returns I/I0"""
+    """ A  pseudo counter which remebers the input for negative magnetic
+    fields and returns it at positive fields"""
 
-    counter_roles        = ('I1', 'I2', 'I3', 'I4', 'I5', 'I6',  'I7',  'I8', )
-    pseudo_counter_roles = ('O1', 'O2', 'O3', 'O4', 'O5', 'O6',  'O7',  'O8',)
-    value = [0,0,0,0,0,0,0,0]
+    counter_roles        = ('I',)
+    pseudo_counter_roles = ('O',)
+    value = 0
     field = 0
     
     def __init__(self, inst, props):  
         PseudoCounterController.__init__(self, inst, props)
-        self.magnet = PyTango.DeviceProxy("motor/caenfastpsctrl/0")
-        
+        self.magnet = taurus.Device("magnet")
+    
     def Calc(self, axis, counters):
-        counter = counters[axis-1]
+        counter = counters[0]
+        self.field = self.magnet.getPosition()
         
-        if axis == 1:
-            self.field = self.magnet.position
-      
         if self.field < 0:
-            self.value[axis-1] = counter        
-
-        return self.value[axis-1]
+            self.value = counter        
+    
+        return self.value
