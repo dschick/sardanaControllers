60bd8c408846d0b78e1247db39e79d3e4af096de
Daniel Schick <schick.daniel@gmail.com>
2019-08-02 17:18:11 +0200

    add filenameing

--- greateyesCounterTimerController.py
+++ greateyesCounterTimerController.py
@@ -22,7 +22,7 @@
 ##############################################################################
 
 from sardana import State
-from sardana.pool.controller import CounterTimerController, Type, Description, DefaultValue
+from sardana.pool.controller import CounterTimerController, Type, Description, DefaultValue, DataAccess, Access, FGet, FSet
 import socket
 import struct
 import json
@@ -59,15 +59,15 @@ class greatEyes:
         #print(ack_bytes)
         
         # Look for the response
-#        amount_received = 0
-#        ack = b''
-#        
-#        while amount_received < ack_bytes:
-#            data = self.s.recv(4096)
-#            amount_received += len(data)
-#            ack += data
+        amount_received = 0
+        ack = b''
         
-        ack = self.s.recv(ack_bytes)
+        while amount_received < ack_bytes:
+            data = self.s.recv(4096)
+            amount_received += len(data)
+            ack += data
+        
+#        ack = self.s.recv(ack_bytes)
         
         return(ack)
     
@@ -89,7 +89,31 @@ class greateyesCounterTimerController(CounterTimerController):
                              Description: 'The port of the greateyes labview server', 
                              DefaultValue: 5000},
                        }
-                        
+    
+    ctrl_attributes = {'PointNb' : { Type : int,
+                                             Description : "pointNb of the scan",
+                                             Access : DataAccess.ReadWrite,
+                                             FGet : 'getPointNb', FSet : 'setPointNb', },
+                        'FileName' : { Type : str,
+                                             Description : "file name of image",
+                                             Access : DataAccess.ReadWrite,
+                                             FGet : 'getFileName', FSet : 'setFileName', },
+                        }
+    
+    def getPointNb(self):
+        return self.pointNb
+    
+    def setPointNb(self, value):
+        self.pointNb = int(value)
+        return
+
+    def getFileName(self):
+        return self.fileName
+    
+    def setFileName(self, value):
+        self.fileName = value
+        return
+                
     def AddDevice(self, axis):
         self._axes[axis] = {}
 
@@ -108,23 +132,31 @@ class greateyesCounterTimerController(CounterTimerController):
         self._axes = {}
         self.timeout = 5
         self.start_time = 0
+        self.pointNb = 0
+        self.fileName = ''
         
     def ReadOne(self, axis):
         """Get the specified counter value"""
         
-        if axis == 0:
+        if (axis == 0) and (self.isAquiring == False):
+            self.data = []
             peaks = json.loads(self.ge.writeRead(b'GET_PEAKS'))
+            print(peaks)
             self.data = np.array(peaks, dtype=float).flatten()
-            
+            print(self.data)
             for spec, ref in zip(self.data[0:10], self.data[10:20]):
-                if ref == 0:
+                if ref == 0: # avoid division by 0
                     self.data = np.append(self.data, 0)
                 else:
                     self.data = np.append(self.data, spec/ref)
                     
             timestamps = json.loads(self.ge.writeRead(b'GET_TIMESTAMPS'))
-            spec_filename = timestamps[0]
-            fileID = np.uint64(spec_filename[5:11] + spec_filename[12:18] + spec_filename[19:22])
+            URIs = json.loads(self.ge.writeRead(b'GET_URI'))
+            print(URIs)
+            print(timestamps)
+#            spec_filename = timestamps[0]
+            fileID = -999#np.uint64(spec_filename[5:11] + spec_filename[12:18] + spec_filename[19:22])
+            print(fileID)
             self.data = np.append(self.data, fileID)
          
         return self.data[axis]
@@ -136,15 +168,19 @@ class greateyesCounterTimerController(CounterTimerController):
         
         if axis == 0:
             res = self.ge.writeRead(b'GET_STATUS')
-            status = int(res.decode("utf-8").split(' ')[1])
-            if (status == -1) or (status == 1):
-                self.isAquiring = False
-            elif status == -2:
-                #print("ERROR: Camera in running mode!")
-                self.error("ERROR: Camera in running mode!")
-                self.warning("ERROR: Camera in running mode!")
-                return State.Fault, "Camera in running mode!"
-            else:
+            try:            
+                status = int(res.decode("utf-8").split(' ')[1])
+                if (status == -1) or (status == 1):
+                    self.isAquiring = False
+                elif status == -2:
+                    #print("ERROR: Camera in running mode!")
+                    self.error("ERROR: Camera in running mode!")
+                    self.warning("ERROR: Camera in running mode!")
+                    return State.Fault, "Camera in running mode!"
+                else:
+                    self.isAquiring = True
+            except:
+                #return State.Fault, "error while parsing status response!"
                 self.isAquiring = True
             
         if self.isAquiring:
@@ -159,7 +195,19 @@ class greateyesCounterTimerController(CounterTimerController):
     
     def StartAll(self):
         self.isAquiring = True
-        self.ge.write(b'ACQUIRE')
+        # create path
+        if self.pointNb < 0: #ct
+            filePath = self.fileName
+        else:
+            filePath = '{:s}{:04d}'.format(self.fileName,self.pointNb)
+            
+        filePath = filePath.replace('/', '\\')
+        if filePath:
+            print(filePath)
+            res = self.ge.writeRead(b'ACQUIRE ' + filePath)
+        else:
+            print('No filePath given')
+            res = self.ge.writeRead(b'ACQUIRE')
         self.start_time = time.time()
         #time.sleep(0.1)
     
@@ -173,7 +221,7 @@ class greateyesCounterTimerController(CounterTimerController):
     def AbortOne(self, axis):
         """Abort the specified counter"""
         pass
-    
+        
     def SendToCtrl(self, cmd):
         """
         Send custom native commands. The cmd is a space separated string
@@ -187,9 +235,17 @@ class greateyesCounterTimerController(CounterTimerController):
         mode = cmd.split(' ')[0].lower()
         args = cmd.strip().split(' ')[1:]
 
-        if mode == "setpath":
-            self.ge.write(b'SET_FILENAME {:}'.format(args[0]))
+        if mode == "set_basepath":
+            self.ge.writeRead(b'SET_BASEPATH {:}'.format(args[0]))
         elif mode == "set_exposure":
-            self.ge.write(b'SET_EXPOSURETIME {:} {:}'.format(args[0], int(float(args[1])*1000)))
+            self.ge.writeRead(b'SET_EXPOSURETIME {:} {:}'.format(args[0], int(float(args[1])*1000)))
         elif mode == "dark":
-            self.ge.write(b'DARK {:}'.format(args[0]))
+            self.ge.writeRead(b'DARK {:}'.format(args[0]))
+        elif mode == "set_save":
+            self.ge.writeRead(b'SET_SAVE {:} {:}'.format(args[0], int(args[1])))
+        elif mode == "set_save_dark":
+            self.ge.writeRead(b'SET_SAVEDARK {:} {:}'.format(args[0], int(args[1])))
+        elif mode == "get_spectra":
+            res = self.ge.writeRead(b'GET_SPECTRUM')
+            print(res)
+            
